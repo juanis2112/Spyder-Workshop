c48d5ae151f0f524857fc94c00644d4598b41c56
juanis2112 <juanitagomezr2112@gmail.com>
2020-02-21 12:51:51 -0500

    Adding Color gradient

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@
 
 # Third-party imports
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_correlations(df, size=10):
@@ -47,3 +48,36 @@ def predicted_temperature(humidity, beta_0, beta_1):
     Uses a linear regression as model: temperature = beta_0 + beta_1 * humidity
     """
     return beta_0 + beta_1 * humidity
+
+cmaps = [('Perceptually Uniform Sequential', [
+            'viridis', 'plasma',])]
+
+
+
+def plot_color_gradients(cmap_category, cmap_list):
+    gradient = np.linspace(0, 1, 256)
+    gradient = np.vstack((gradient, gradient))
+    # Create figure and adjust figure height to number of colormaps
+    nrows = len(cmap_list)
+    figh = 0.35 + 0.15 + (nrows + (nrows-1)*0.1)*0.22
+    fig, axes = plt.subplots(nrows=nrows, figsize=(6.4, figh))
+    fig.subplots_adjust(top=1-.35/figh, bottom=.15/figh, left=0.2, right=0.99)
+
+    axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
+    
+    print(axes)
+    print(cmap_list)
+    for ax, name in zip(axes, cmap_list):
+        ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(name))
+        ax.text(-.01, .5, name, va='center', ha='right', fontsize=10,
+                transform=ax.transAxes)
+
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axes:
+        ax.set_axis_off()
+
+
+for cmap_category, cmap_list in cmaps:
+    plot_color_gradients(cmap_category, cmap_list)
+
+plt.show()
