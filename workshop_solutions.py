c1edb4405961ca130ebb69dd9ffa7713494fad43
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2020-07-02 03:04:44 -0500

    Implement basic code improvements and fixes to solutions

--- workshop_solutions.py
+++ workshop_solutions.py
@@ -10,22 +10,22 @@
 # Third-party imports
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import explained_variance_score
+from sklearn.model_selection import train_test_split
 
 # Local imports
-from utils import (
-    plot_correlations, plot_color_gradients, aggregate_by_year,
-    predicted_temperature)
+from utils import aggregate_by_year, plot_correlations, plot_color_gradients
+
 
 # In[2] Exploring Data
 
 # Reading data
 weather_data = pd.read_csv('data/weatherHistory.csv')
 
-# Print size of data 
+# Print size of data
 print(len(weather_data))
+
 # Print first 3 rows of DataFrame
 print(weather_data.head(3))
 
@@ -37,29 +37,31 @@ print(weather_data.tail(3))
 
 # Order rows according to date
 weather_data['Formatted Date'] = pd.to_datetime(
-    weather_data['Formatted Date'])
+    weather_data['Formatted Date'], format='%Y-%m-%d %H:%M:%S.%f %z')
 weather_data_ordered = weather_data.sort_values(by='Formatted Date')
+
 # Order Index according to date
-weather_data_ordered = weather_data_ordered.reset_index(drop=True)
+weather_data_ordered.reset_index(drop=True, inplace=True)
+
 # Drop categorical columns
-weather_data_ordered = weather_data_ordered.drop(
-    columns=['Summary', 'Precip Type', 'Loud Cover', 'Daily Summary'])
+weather_data_ordered.drop(
+    columns=['Summary', 'Precip Type', 'Loud Cover', 'Daily Summary'],
+    inplace=True)
+
 # Plot Temperature Vs Formatted Date
 weather_data_ordered.plot(
-    x='Formatted Date', y=['Temperature (C)'], color='red', figsize=(15, 8))
+    x='Formatted Date', y='Temperature (C)', color='red', figsize=(15, 8))
 
 # TO DO: Plot Temperature (C) V.S the Date using only the data from 2006
 weather_data_ordered.head(8759).plot(x='Formatted Date', y=['Temperature (C)'], color='red')
 
-# -----------------------------------------------------------------------------
 # Plot Temperature and Humidity in the same plot
 weather_data_ordered.plot(
     subplots=True, x='Formatted Date', y=['Temperature (C)', 'Humidity'],
     figsize=(15, 8))
+
 # TO DO: Plot different combinations of the variables, for different years
 
-
-# -----------------------------------------------------------------------------
 
 # In[4] Data summarization and aggregation
 
@@ -75,11 +77,12 @@ weather_data_by_year = aggregate_by_year(
 
 # Plot Correlations
 plot_correlations(weather_data_ordered, size=15)
-# Plot Gradients colormaps
-cmap_category, cmap_list = ('Plot gradients convention', ['viridis', ])
-plot_color_gradients(cmap_category, cmap_list)
 
-# Compute Correlations 
+# Plot Gradients colormaps
+plot_color_gradients(
+    cmap_category='Plot gradients convention', cmap_list=['viridis', ])
+
+# Compute Correlations
 weather_correlations = weather_data_ordered.corr()
 weather_data_ordered['Temperature (C)'].corr(
     weather_data_ordered['Humidity'])
@@ -91,32 +94,27 @@ weather_data_ordered['Temperature (C)'].corr(
 # In[6] Data Modeling and Prediction
 
 # Get data subsets for the model
-X_train, X_test, Y_train, Y_test = train_test_split(
+x_train, x_test, y_train, y_test = train_test_split(
     weather_data_ordered['Humidity'], weather_data_ordered['Temperature (C)'],
     test_size=0.25)
 
 # Run regression
-regresion = linear_model.LinearRegression()
-regresion.fit(X_train.values.reshape(-1, 1), Y_train.values.reshape(-1, 1))
-print(regresion.intercept_, regresion.coef_)  # beta_0=intercept, beta_1=coef_
+regression = linear_model.LinearRegression()
+regression.fit(x_train.values.reshape(-1, 1), y_train.values.reshape(-1, 1))
 
-# Get coefficients
-beta_0 = regresion.intercept_[0]
-beta_1 = regresion.coef_[0, 0]
+# Print coefficients
+print(regression.intercept_, regression.coef_)  # beta_0, beta_1
 
-# Plot predicted model with test data.
-Y_predict = predicted_temperature(X_test, beta_0, beta_1)
-plt.scatter(X_test, Y_test, c='red', label='observation', s=1)
-plt.scatter(X_test, Y_predict, c='blue', label='model')
+# Plot predicted model with test data
+y_predict = regression.predict(x_test.values.reshape(-1, 1))
+plt.scatter(x_test, y_test, c='red', label='Observation', s=1)
+plt.scatter(x_test, y_predict, c='blue', label='Model')
 plt.xlabel('Humidity')
 plt.ylabel('Temperature (C)')
 plt.legend()
 plt.show()
 
-# TO DO: Using the coefficients predict the temperature for a
-#       given level of humidity using the 'predicted_temperature' function
-#       available in 'utils'
+# TO DO: Using the model, predict the temperature for a given level of humidity
 
 # Evaluate model numerically
-ev = explained_variance_score(Y_test, Y_predict)
-print(ev)
+print(explained_variance_score(y_test, y_predict))
